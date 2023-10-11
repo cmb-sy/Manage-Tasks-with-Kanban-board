4876703a0dc5ecfb631818f9d81a8a731fc5252c
nakashima1125 <snakashima@armg.jp>
2023-10-11 09:36:21 +0900

    フォーカス時に活性化させる機能を追加

--- kanban-task/src/components/ListAdd.vue
+++ kanban-task/src/components/ListAdd.vue
@@ -1,23 +1,43 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { useStore } from "vuex"; // useStore フックを追加
 
 let title = ref("");
+let isEditing = ref(false);
 const store = useStore(); // ストアを取得
 
 const addList = () => {
   store.dispatch("addList", { title: title.value }); // title.value として参照
   title.value = ""; // title.value として更新
 };
+
+// フォーカスされる：ユーザビリティなクリックの実現
+const startEditing = () => {
+  isEditing.value = true;
+};
+
+const finishEditing = () => {
+  isEditing.value = false;
+};
+
+const classes = computed(() => {
+  const classList = ["addlist"];
+  if (isEditing.value) {
+    classList.push("active");
+  }
+  return classList;
+});
 </script>
 
 <template>
-  <form class="addlist" @submit.prevent="addList">
+  <form :class="classes" @submit.prevent="addList">
     <input
       v-model="title"
       type="text"
       class="text-input"
       placeholder="Add new list"
+      @focusin="startEditing"
+      @focusout="finishEditing"
     />
     <button type="submit" class="add-button">Add</button>
   </form>
